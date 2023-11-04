05cd774aa2b5b85937415e2cb5b4d7414faf700a
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-11-04 18:12:27 +0300

    feat: rewrite app-component on function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,63 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
 import reactLogo from './assets/react.svg';
 import SearchInfo from './components/search-input/search-input';
 import Results from './components/result-component/result-component';
 import { ResponseResult, InfoData } from './types/response-interface';
 
-interface AppComponentState {
-  data: ResponseResult[];
-  info: InfoData | null;
-  isLoading: boolean;
-  hasError: boolean;
-}
+function App() {
+  const [data, setData] = useState<ResponseResult[]>([]);
+  const [info, setInfo] = useState<InfoData | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
-class App extends Component {
-  state: AppComponentState = {
-    data: [],
-    info: null,
-    isLoading: false,
-    hasError: false,
+  const handleResponse = (results: ResponseResult[], info: InfoData | null) => {
+    setData(results);
+    setInfo(info);
   };
 
-  handleResponse = (results: ResponseResult[], info: InfoData | null) => {
-    this.setState({
-      data: results,
-      info: info,
-    });
+  const handleLoading = (loading: boolean) => {
+    setIsLoading(loading);
   };
 
-  handleLoading = (loading: boolean) => {
-    this.setState({
-      isLoading: loading,
-    });
+  const createError = () => {
+    setHasError(true);
   };
 
-  createError = () => {
-    this.setState({
-      hasError: true,
-    });
-  };
-
-  render() {
-    if (this.state.hasError) {
+  useEffect(() => {
+    if (hasError) {
       throw new Error("It's error from click button");
     }
-    return (
-      <>
-        <div>
-          <SearchInfo
-            onResponse={this.handleResponse}
-            data={this.state.data}
-            info={this.state.info}
-            isLoading={this.state.isLoading}
-            onLoading={this.handleLoading}
-          />
-        </div>
-        <div className="result">
-          {this.state.isLoading ? (
-            <div className="result__loading">
-              <p>...Loading</p>
-              <img src={reactLogo} className="logo" alt="React logo" />
-            </div>
-          ) : (
-            <Results data={this.state.data} info={this.state.info} />
-          )}
-          <div className="error-block">
-            <button className="error-block__button" onClick={this.createError}>
-              Error
-            </button>
+  }, [hasError]);
+
+  return (
+    <>
+      <div>
+        <SearchInfo
+          onResponse={handleResponse}
+          data={data}
+          info={info}
+          isLoading={isLoading}
+          onLoading={handleLoading}
+        />
+      </div>
+      <div className="result">
+        {isLoading ? (
+          <div className="result__loading">
+            <p>...Loading</p>
+            <img src={reactLogo} className="logo" alt="React logo" />
           </div>
+        ) : (
+          <Results data={data} info={info} />
+        )}
+        <div className="error-block">
+          <button className="error-block__button" onClick={createError}>
+            Error
+          </button>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default App;
