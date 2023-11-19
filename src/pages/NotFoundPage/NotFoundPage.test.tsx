96ca4d75720422f77279d2fcaf93cffe377c0832
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-11-20 00:48:33 +0300

    feat: correct test not found page

--- src/pages/NotFoundPage/NotFoundPage.test.tsx
+++ src/pages/NotFoundPage/NotFoundPage.test.tsx
@@ -1,10 +1,36 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import {
+  MemoryRouter,
+  RouterProvider,
+  createMemoryRouter,
+} from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
+import MainPage from '../MainPage/MainPage';
 
 describe('<NotFoundPage />', () => {
   it('Should be defined', () => {
     expect(<NotFoundPage />).toBeDefined();
+  });
+
+  it('Ensure that the 404 page is displayed when navigating to an invalid route', async () => {
+    const routes = [
+      {
+        element: <MainPage />,
+        path: '/beer',
+      },
+      {
+        element: <NotFoundPage />,
+        path: '*',
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/foo'],
+    });
+
+    render(<RouterProvider router={router} />);
+    expect(screen.getByText('Page not found')).toBeInTheDocument();
+    expect(screen.getByText('Back')).toBeInTheDocument();
   });
 
   it('Should be navigate a the beer page after click', async () => {
