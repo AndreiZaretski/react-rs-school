55917db9f5ccb7b06d93bc4771391458e7ef2c4a
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-12-03 19:06:13 +0300

    feat: change the interface order to display elements correctly

--- src/model/formModel.ts
+++ src/model/formModel.ts
@@ -1,13 +1,13 @@
 export interface FormModel {
-  picture: string | ArrayBuffer | null | FileList | File;
   name: string | undefined;
+  picture: string | ArrayBuffer | null | FileList | File;
   age: number | string | undefined;
   email: string | undefined;
+  gender: string | undefined;
+  country: string | undefined;
   password: string | undefined;
   confirmPassword: string | undefined;
-  gender: string | undefined;
   acceptTerms: boolean | undefined;
-  country: string | undefined;
 }
 
 export interface FormPrintModel extends FormModel {
